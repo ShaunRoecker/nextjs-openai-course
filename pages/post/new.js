ffff9a90d74b5a267b66eeff6456d6972b2abfd3
s-roecker <shaun.roecker@gmail.com>
2023-05-10 22:53:22 -0400

    add openai api

--- pages/post/new.js
+++ pages/post/new.js
@@ -3,9 +3,20 @@ import { AppLayout } from "../../components/AppLayout";
 
 const NewPost = (props) => {
     console.log("NEW POST PROPS", props)
+    const handleClick = async () => {
+        const response = fetch("/api/generatePost", {
+            method: "POST"
+        })
+        const json = (await response).json();
+        console.log("RESULT ", json);
+    };
+
     return (
         <div className=""> 
-            New post page
+            <h1>This is a new post page</h1>
+            <button className="btn" onClick={handleClick}>
+                Generate
+            </button>
         </div>
     );
 }
