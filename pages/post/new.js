18377013e1d1085921da9d249b5fc8ad80fc43ed
s-roecker <shaun.roecker@gmail.com>
2023-05-14 01:22:28 -0400

    got openai generatePost endpoints working

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,22 +1,69 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
+
 
 const NewPost = (props) => {
-    console.log("NEW POST PROPS", props)
-    const handleClick = async () => {
-        const response = fetch("/api/generatePost", {
-            method: "POST"
+    // console.log("NEW POST PROPS", props)
+    const [postContent, setPostContent] = useState("");
+    const [topic, setTopic] = useState("");
+    const [keywords, setKeywords] = useState("");
+    const [title, setTitle] = useState("");
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const response = await fetch("/api/generatePost", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ topic, keywords })
+           
+
         })
-        const json = (await response).json();
-        console.log("RESULT ", json);
+        const json = await response.json();
+        const { postContent, title, metaDescription} = json.post
+        console.log("RESULT ", title, postContent, metaDescription );
+        setPostContent(postContent)
+        setTitle(title)
     };
 
     return (
         <div className=""> 
-            <h1>This is a new post page</h1>
-            <button className="btn" onClick={handleClick}>
-                Generate
-            </button>
+            {title? <h1>{title}</h1> : <h1>This is a new post page</h1>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        <strong>
+                            Generate a blog post on the topic of:
+                        </strong>
+                    </label>
+                    <textarea 
+                        className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 round" 
+                        value={topic} 
+                        onChange={(e) => setTopic(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>
+                        <strong>
+                            Targeting the following keywords:
+                        </strong>
+                    </label>
+                    <textarea 
+                        className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 round"
+                        value={keywords} 
+                        onChange={(e) => setKeywords(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit" className="btn">
+                    Generate
+                </button>
+            </form>
+            
+            <div className="max-w-screen-sm p-10" dangerouslySetInnerHTML={{__html: postContent}} />
         </div>
     );
 }
